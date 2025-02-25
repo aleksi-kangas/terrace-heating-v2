50e162108ebaa8c40d48f03ab564fe98678af507
Aleksi Kangas <aleksi.kangas@hotmail.com>
2025-02-25 18:21:36 +0200

    HeatPumpSnapshot controller with endpoints

---
+++ backend/src/main/java/com/github/aleksikangas/backend/controller/heatpump/HeatPumpSnapshotController.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2025 Aleksi Kangas.
+ */
+
+package com.github.aleksikangas.backend.controller.heatpump;
+
+import com.github.aleksikangas.backend.domain.snapshot.HeatPumpSnapshot;
+import com.github.aleksikangas.backend.persistence.repositories.HeatPumpSnapshotRepository;
+import jakarta.validation.constraints.NotNull;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Serves {@link HeatPumpSnapshot}s for specific time intervals.
+ */
+@RequestMapping("heat-pump/snapshots")
+@RestController
+public final class HeatPumpSnapshotController {
+
+  private final HeatPumpSnapshotRepository heatPumpSnapshotRepository;
+
+  public HeatPumpSnapshotController(@Autowired final HeatPumpSnapshotRepository heatPumpSnapshotRepository) {
+    this.heatPumpSnapshotRepository = heatPumpSnapshotRepository;
+  }
+
+  @GetMapping
+  public ResponseEntity<List<HeatPumpSnapshot>> listSnapshotsFromTo(
+      @DateTimeFormat(iso = ISO.DATE_TIME) @NotNull @RequestParam final Instant from,
+      @DateTimeFormat(iso = ISO.DATE_TIME) @NotNull @RequestParam final Instant to) {
+    if (from.isBefore(to)) {
+      return ResponseEntity.badRequest().build();
+    }
+    if (from.until(to).toDays() > 30) {
+      return ResponseEntity.badRequest().build();
+    }
+    return ResponseEntity.ok(heatPumpSnapshotRepository.findByTimestampBetweenOrderByTimestamp(from, to));
+  }
+
+  @GetMapping("days/{days}")
+  public ResponseEntity<List<HeatPumpSnapshot>> listSnapshotsForPreviousDays(@PathVariable final int days) {
+    if (days < 1 || days > 30) {
+      return ResponseEntity.badRequest().build();
+    }
+    final Instant threshold = Instant.now().minus(days, ChronoUnit.DAYS);
+    return ResponseEntity.ok(heatPumpSnapshotRepository.findByTimestampAfterOrderByTimestamp(threshold));
+  }
+}
--- backend/src/main/java/com/github/aleksikangas/backend/persistence/repositories/HeatPumpSnapshotRepository.java
+++ backend/src/main/java/com/github/aleksikangas/backend/persistence/repositories/HeatPumpSnapshotRepository.java
@@ -5,6 +5,9 @@
 package com.github.aleksikangas.backend.persistence.repositories;
 
 import com.github.aleksikangas.backend.domain.snapshot.HeatPumpSnapshot;
+import jakarta.annotation.Nonnull;
+import java.time.Instant;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -14,4 +17,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface HeatPumpSnapshotRepository extends JpaRepository<HeatPumpSnapshot, Long> {
 
+  List<HeatPumpSnapshot> findByTimestampAfterOrderByTimestamp(@Nonnull Instant after);
+
+  List<HeatPumpSnapshot> findByTimestampBetweenOrderByTimestamp(@Nonnull Instant from, @Nonnull Instant to);
 }
