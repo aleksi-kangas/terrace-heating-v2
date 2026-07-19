0bff8f3c907a9108dfa212d251209189eea3dbfc
Aleksi Kangas <aleksi.kangas@hotmail.com>
2026-07-19 16:58:16 +0300

    Improved NavBar

--- ui/app/components/NavBar.tsx
+++ ui/app/components/NavBar.tsx
@@ -5,53 +5,59 @@ import Link from "next/link";
 import React from "react";
 import {usePathname} from "next/navigation";
 
-interface NavbarItemProps {
-    active: boolean;
-    href: string;
-    label: string;
+interface NavBarItemProps {
+  active: boolean;
+  href: string;
+  label: string;
 }
 
-const NavBarItem = ({active, href, label}: NavbarItemProps) => {
-    return (
-        <Link href={href}>
-            <Button fw={500} size="compact-md" variant={active ? "default" : "subtle"}>
-                {label}
-            </Button>
-        </Link>
-    )
+const NavBarItem = ({active, href, label}: NavBarItemProps) => {
+  return (
+      <Link href={href}>
+        <Button
+            variant={active ? "light" : "default"}
+            color="blue"
+            radius="l"
+            fw={600}
+            size="sm"
+        >
+          {label}
+        </Button>
+      </Link>
+  )
 }
 
 const NavBar = () => {
-    const pathname = usePathname();
+  const pathname = usePathname();
 
-    const navbarItems = [
-        {
-            href: '/dashboard',
-            label: "Dashboard",
-        },
-        {
-            href: '/charts',
-            label: "Charts",
-        },
-        {
-            href: '/timers',
-            label: "Timers",
-        }
-    ];
+  const navBarItems = [
+    {
+      href: '/dashboard',
+      label: "Dashboard",
+    },
+    {
+      href: '/charts',
+      label: "Charts",
+    },
+    {
+      href: '/timers',
+      label: "Timers",
+    }
+  ];
 
-    return (
-        <Group h="100%" px="md" style={{flex: 1}}>
-            <IconTemperature size={30}/>
-            <Group gap={10}>
-                {navbarItems.map((item) => (
-                    <NavBarItem key={item.href}
-                                active={pathname.startsWith(item.href)}
-                                href={item.href}
-                                label={item.label}/>
-                ))}
-            </Group>
+  return (
+      <Group h="100%" px="md" style={{flex: 1}}>
+        <IconTemperature size={30}/>
+        <Group gap={10}>
+          {navBarItems.map((item) => (
+              <NavBarItem key={item.href}
+                          active={pathname.startsWith(item.href)}
+                          href={item.href}
+                          label={item.label}/>
+          ))}
         </Group>
-    )
+      </Group>
+  )
 }
 
 export default NavBar;
